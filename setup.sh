b6c866cd44aae9b705b47e0d0cee3e8dbc67cd1d
trieulong <toannt25@fsoft.com.vn>
2022-09-13 13:40:02 +0700

    temp

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#export DATABASE_URL="postgres://zxlcgvseepskdu:c0b06be3f7221d941f1fa84b25d72b77dc1f18f94698c67b7260a55a8dfd4c3c@ec2-54-87-179-4.compute-1.amazonaws.com:5432/d7pj98rs21j2j1"
+export DATABASE_URL="postgres://zxlcgvseepskdu:c0b06be3f7221d941f1fa84b25d72b77dc1f18f94698c67b7260a55a8dfd4c3c@ec2-54-87-179-4.compute-1.amazonaws.com:5432/d7pj98rs21j2j1"
 export EXCITED="true"
 export AUTH0_DOMAIN="dev-88qykzd8.us.auth0.com"
 export AUTH0_CLIENT_ID="9ISBCj7RxXxwaVgCW0ebDt6475KIcsd2"
