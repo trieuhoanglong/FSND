9d58618848202da8755244e31919ddba709aaf60
trieulong <toannt25@fsoft.com.vn>
2022-09-13 13:48:12 +0700

    temp

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 export DATABASE_URL="postgres://zxlcgvseepskdu:c0b06be3f7221d941f1fa84b25d72b77dc1f18f94698c67b7260a55a8dfd4c3c@ec2-54-87-179-4.compute-1.amazonaws.com:5432/d7pj98rs21j2j1"
 export EXCITED="true"
 export AUTH0_DOMAIN="dev-88qykzd8.us.auth0.com"
